14b6a8bb2dc841fb5f9f9fbabe7fcb1b314ddd21
Dexlaer <Dexlaer@users.noreply.github.com>
2026-05-17 17:38:30 +0300

    Add PWA app icons and manifest

---
+++ sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = "englishlab-cache-v1";
+
+const CORE_ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./pronouns.html",
+  "./app.js",
+  "./phrase-switch.html",
+  "./phrase-switch.css",
+  "./phrase-switch.js",
+  "./do-play-go.html",
+  "./do-play-go.css",
+  "./do-play-go.js",
+  "./confusing-forms.html",
+  "./confusing-forms.css",
+  "./confusing-forms.js",
+  "./manifest.webmanifest",
+  "./icons/favicon-32.png",
+  "./icons/apple-touch-icon.png",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
